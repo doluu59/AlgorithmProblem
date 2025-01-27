9f3083cd470651d94e9838de5e255873ba083d8e
유연석 <ysyoo627@naver.com>
2025-01-27 22:45:28 +0900

    add: PG_완주하지못한선수 (JAVA, 해쉬)

---
+++ JAVA/Hash/Programmers/hihg_score_kit/PG_lv1_완주하지못한선수.java
@@ -0,0 +1,48 @@
+/**
+ * @from Programmers
+ * @index
+ * @title 완주하지 못한 선수
+ * @grade Level 1
+ * @classification 해쉬
+ */
+
+package JAVA.Hash.Programmers.hihg_score_kit;
+
+import java.util.*;
+
+public class PG_lv1_완주하지못한선수 {
+    public String solution(String[] participant, String[] completion) {
+        String answer = null;
+        Map<String, Integer> map = new HashMap<>();
+        for (String part:participant) {
+            if(map.containsKey(part)) {
+                map.put(part, map.get(part)+1);
+            } else {
+                map.put(part, 1);
+            }
+        }
+        for (String comp: completion) {
+            map.put(comp, map.get(comp)-1);
+        }
+        for (String key : map.keySet()) {
+            if (map.get(key) != 0) {
+                answer = key;
+                break;
+            }
+        }
+
+//         Set<String> set = new HashSet<>();
+//         for (String part : participant) {
+//             set.add(part);
+//         }
+//         for (String comp : completion) {
+//             set.remove(comp);
+//         }
+
+//         for (String str : set) {
+//             answer = str;
+//         }
+        return answer;
+    }
+}
+
