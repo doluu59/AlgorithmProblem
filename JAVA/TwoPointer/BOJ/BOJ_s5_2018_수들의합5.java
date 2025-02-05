4c2618c2e7059d8747643abf354b0955fff0d0b7
유연석 <ysyoo627@naver.com>
2025-02-05 19:44:45 +0900

    add : BOJ_s5_2018_수들의합5 (JAVA, 투 포인터)

---
+++ JAVA/TwoPointer/BOJ/BOJ_s5_2018_수들의합5.java
@@ -0,0 +1,36 @@
+package JAVA.TwoPointer.BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @from BOJ
+ * @index 2018
+ * @title 수들의 합 5
+ * @grade Silver 5
+ * @classification 투 포인터
+ * @solve
+ */
+public class BOJ_s5_2018_수들의합5 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 1;
+        int startIndex = 1;
+        int endIndex = 1;
+        int sum = 1;
+        while (endIndex < N) {
+            if (sum < N) {
+                sum += ++endIndex;
+            } else if (sum > N) {
+                sum -= startIndex++;
+            } else {
+                count++;
+                sum += ++endIndex;
+            }
+        }
+        System.out.println(count);
+    }
+}
