cc4643dd5560e39a9f2c2da837e63289543b7e1b
유연석 <ysyoo627@naver.com>
2025-03-19 00:03:04 +0900

    chore : PG_lv2_PCCP_퍼즐게임챌린지 (JAVA, BinarySearch)

--- JAVA/Search/BinarySearch/PG_lv2_PCCP_퍼즐게임챌린지.java
+++ JAVA/Search/BinarySearch/PG_lv2_PCCP_퍼즐게임챌린지.java
@@ -19,15 +19,17 @@ public class PG_lv2_PCCP_퍼즐게임챌린지 {
             int length = diffs.length;
 
             // idea : 이분탐색?
-            // 중앙값 어케 고르지.. limit 값 ? diff 값? -> 가장 어려운 퍼즐도 한번에 풀 수 있으면 숙련도 더 높아져도 상관 X
-            int minLevel = 1;
+            int minLevel = 1;   // 양수 제한
+            // 숙련도 최대값 = diff 값 중 최대 : 가장 어려운 퍼즐도 한번에 풀 수 있으면 숙련도 더 높아져도 같은 시간 소요
             int maxLevel = Integer.MIN_VALUE;
             for (int diff : diffs) {
                 if (diff > maxLevel) maxLevel = diff;
             }
+            // Binary Search : 숙련도의 최대값과 최소값의 중간값으로 해결 가능한 지 판단 후 범위 줄이기
             while (minLevel <= maxLevel) {
-                int level = (minLevel + maxLevel) / 2;
+                int level = (minLevel + maxLevel) / 2;  // 숙련도 중간값
                 long time = times[0];
+                // 소요 시간 계산
                 for (int i=1; i<length; i++) {
                     if (diffs[i] <= level) {
                         time += times[i];
@@ -35,17 +37,15 @@ public class PG_lv2_PCCP_퍼즐게임챌린지 {
                         time += (diffs[i] - level) * (times[i] + times[i-1] ) + times[i];
                     }
                 }
-
+                // 미션 성공인지
                 if (time <= limit) {    // clear
                     maxLevel = level - 1;
                 } else {                // fail
                     minLevel = level + 1;
                 }
             }
-
             answer = minLevel;
             return answer;
         }
     }
-
 }
