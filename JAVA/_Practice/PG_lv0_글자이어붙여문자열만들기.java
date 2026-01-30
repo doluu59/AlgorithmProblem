09247879cfb95fc8aa69f4c7c3430d389111be2b
유연석 <ysyoo627@gmail.com>
2026-01-30 23:19:35 +0900

    add: PG_lv0_글자이어붙여문자열만들기 (Practice)

--- JAVA/_Practice/PG_lv0_글자이어붙여문자열만들기.java
+++ JAVA/_Practice/PG_lv0_글자이어붙여문자열만들기.java
@@ -4,7 +4,7 @@ public class PG_lv0_글자이어붙여문자열만들기 {
     public static void main(String[] args) {
         class Solution {
             public String solution(String my_string, int[] index_list) {
-                StringBuffer sb = new StringBuffer();
+                StringBuilder sb = new StringBuilder();
                 for(int index : index_list) {
                     sb.append(my_string.charAt(index));
                 }
