c0a4ad223a98616c60c28652cd4b32dbfdf4b90a
유연석 <ysyoo627@gmail.com>
2026-02-22 23:17:13 +0900

    chore: PG_lv0_뒤에서5등까지 (Practice)

--- JAVA/_Practice/PG_lv0_뒤에서5등까지.java
+++ JAVA/_Practice/PG_lv0_뒤에서5등까지.java
@@ -2,17 +2,6 @@ package JAVA._Practice;
 
 import java.util.Arrays;
 
-/**
- * @author |   ysyoo
- * @platform |   문제 출처 (Baekjoon / Programmers / LeetCode / Softeer ...)
- * @index |   문제 번호
- * @title |   제목
- * @link |   문제 링크
- * @level |   난이도 (Gold 5, Lv.2, Medium ...)
- * @approach |   한 줄 문제 요약
- * @algorithm |   문제 유형 (DP, Graph, Simulation ...)
- * @technique |   사용된 기법 (Bit masking, stream ...)
- */
 public class PG_lv0_뒤에서5등까지 {
     public static void main(String[] args) {
         class Solution {
