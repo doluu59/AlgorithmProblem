64fa426b0d15f0aca75dac53c8f56e1ac9a90004
유연석 <ysyoo627@gmail.com>
2026-03-17 23:26:34 +0900

    chore: rename BOJ_1152_B2_단어의개수

--- JAVA/_Practice/BOJ_B2_1152_단어의개수.java
+++ JAVA/_Practice/BOJ_B2_1152_단어의개수.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ_1152_B2_단어의개수 {
+public class BOJ_B2_1152_단어의개수 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
