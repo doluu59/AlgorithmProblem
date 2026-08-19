db7a7c264d6b928e6b676d63c1504477fda57ed3
유연석 <ysyoo627@gmail.com>
2026-08-19 22:56:20 +0900

    add: solve PG_lv0_기초_간단한논리연산 (Practice)

---
+++ JAVA/_Practice/PG_lv0_기초_간단한논리연산.java
@@ -0,0 +1,24 @@
+package JAVA._Practice;
+
+/**
+ * [PG] 기초. 간단한논리연산 (lv0)
+ * @category Algorithm / Simulation
+ * @implNote
+ * - 문제 요약   : 
+ * - 풀이 전략   : 
+ * - 시간 복잡도 : O()
+ *
+ * @see      <a href="">Problem Link</a>
+ * @author   ysyoo
+ * @since    26. 8. 19.
+ */
+ 
+public class PG_lv0_기초_간단한논리연산 {
+	public static void main(String[] args){
+	}
+	class Solution {
+		public boolean solution(boolean x1, boolean x2, boolean x3, boolean x4) {
+			return (x1 || x2) && (x3 || x4);
+		}
+	}
+}
