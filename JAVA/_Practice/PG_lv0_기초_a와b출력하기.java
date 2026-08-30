289fb6082b5921e99e534ee4952a076f28f07bca
유연석 <ysyoo627@gmail.com>
2026-08-30 22:12:54 +0900

    add : solve PG_lv0_기초_a와b출력하기 (Practice)

---
+++ JAVA/_Practice/PG_lv0_기초_a와b출력하기.java
@@ -0,0 +1,27 @@
+package JAVA._Practice;
+
+/**
+ * [PG] 기초. a와b출력하기 (lv0)
+ * @category Algorithm / Simulation
+ * @implNote
+ * - 문제 요약   : 
+ * - 풀이 전략   : 
+ * - 시간 복잡도 : O()
+ *
+ * @see      <a href="">Problem Link</a>
+ * @author   ysyoo
+ * @since    26. 8. 30.
+ */
+
+import java.util.Scanner;
+
+public class PG_lv0_기초_a와b출력하기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+	}
+}
