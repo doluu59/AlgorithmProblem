efe59ad2abd312751f6e00a0ed5c3b6891a491b9
유연석 <ysyoo627@gmail.com>
2026-09-10 23:11:15 +0900

    add : solve PG_lv0_PCCE_1번_출력 (Practice)

---
+++ JAVA/_Practice/PG_lv0_PCCE_1번_출력.java
@@ -0,0 +1,29 @@
+package JAVA._Practice;
+
+/**
+ * [PG] PCCE. 1번 (lv0)
+ * @category Algorithm / Simulation
+ * @implNote
+ * - 문제 요약   : 
+ * - 풀이 전략   : 
+ * - 시간 복잡도 : O()
+ *
+ * @see      <a href="">Problem Link</a>
+ * @author   ysyoo
+ * @since    26. 9. 10.
+ */
+
+import java.util.Scanner;
+
+public class PG_lv0_PCCE_1번_출력 {
+	public static void main(String[] args) {
+		String msg = "Spring is beginning";
+		int val1 = 3;
+		String val2 = "3";
+
+		System.out.println(msg);
+		System.out.println(val1 + 10);
+		System.out.println(val2 + "10");
+
+	}
+}
