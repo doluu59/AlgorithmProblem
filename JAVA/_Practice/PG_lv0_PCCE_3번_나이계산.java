602e6807bd5f285809dfc69eb5c8d3aacc391532
유연석 <ysyoo627@gmail.com>
2026-09-17 23:07:13 +0900

    fix: solve PG_lv0_PCCE_3번_나이계산 (Practice)

--- JAVA/_Practice/PG_lv0_PCCE_3번_나이계산.java
+++ JAVA/_Practice/PG_lv0_PCCE_3번_나이계산.java
@@ -16,24 +16,18 @@ package JAVA._Practice;
 import java.util.Scanner;
 
 public class PG_lv0_PCCE_3번_나이계산  {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		String age_type = sc.next();
+		int answer = 0;
 
-	public class Solution {
-		public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-			int year = sc.nextInt();
-			String age_type = sc.next();
-			int answer = 0;
-
-			if (age_type.equals(
-							"Korea"
-			)) {
-				answer = 2030 - year + 1;
-			}
-			else if (age_type.equals("Year")) {
-				answer = 2030 - year;
-			}
-
-			System.out.println(answer);
+		if (age_type.equals("Korea")) {
+			answer = 2030 - year + 1;
 		}
+		else if (age_type.equals("Year")) {
+			answer = 2030 - year;
+		}
+		System.out.println(answer);
 	}
 }
